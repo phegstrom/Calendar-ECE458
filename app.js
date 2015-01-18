e362e4b784db1625e9092b38e4ad417ce7954e62
phegstrom <parker.hegstrom@gmail.com>
2015-01-17 20:10:25 -0500

    added mongoose, make sure to npm install, still working out mongoose kinks

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// added by Parker
+var mongoose = require('mongoose');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,6 +28,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// added by Parker
+// mongoose.connect('mongodb://localhost/Calender', function(err) {
+//     if(err) {
+//         console.log('connection error', err);
+//     } else {
+//         console.log('connection successful');
+//     }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

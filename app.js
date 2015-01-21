c04f02e6b4306345a571f45675589fb6abef4c03
Peter <ypeter999@gmail.com>
2015-01-21 16:13:30 -0500

    beginning to add mongoose models

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var fs = require('fs');
 
 var app = express();
 
@@ -36,6 +37,20 @@ mongoose.connect('mongodb://localhost/Calender', function(err) {
     } else {
         console.log('connection to local DB successful');
     }
+});
+
+mongoose.model('User', {name: String});
+
+//load all files in models dir
+fs.readdirSync(__dirname + '/models').forEach(function(filename) {
+  if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+});
+
+
+app.get('/User', function(req, res) {
+    mongoose.model('User').find(function(err, User) {
+        res.send(User);
+    });
 });
 
 // catch 404 and forward to error handler

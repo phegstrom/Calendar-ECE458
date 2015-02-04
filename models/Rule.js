8bdefe6c0d8522e0c501cdbd879f8de641bb863c
phegstrom <parker.hegstrom@gmail.com>
2015-02-03 21:51:02 -0500

    RULE now works for PUSH lol

--- models/Rule.js
+++ models/Rule.js
@@ -5,6 +5,8 @@ var collectionName = 'rulesC';
 // for the enum type
 var states = 'canView canViewBusy canNotView'.split(' ');
 
+console.log(states);
+
 var RuleSchema = new Schema({
 	ruleType: {type: String, enum: states},
 	assocUsers: [{type: Schema.Types.ObjectId, ref: 'User'}],
@@ -22,5 +24,17 @@ RuleSchema.methods.addUserGroupViewBusy = function (cb) {
 	//this.canView.push(ObjectID of each user);
 	//this.save(cb);
 };
+
+// RuleSchema.methods.getAllUsersInRule = function (cb) {
+// 	var toRet = [];
+// 	for (var i = 0; i < this.assocUsers.length; i++) {
+// 		toRet.push(this.assocUsers[i].toString());
+// 	}
+
+// 	return toRet;
+// 	// for (var i = 0; i < this.assocUserGroups.length; i++) {
+
+// 	// }
+// };
 
 module.exports = mongoose.model('Rule', RuleSchema);

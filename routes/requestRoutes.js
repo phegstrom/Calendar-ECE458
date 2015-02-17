04808cbfa75d09605fb4096ea054c3f6e0ac27e0
phegstrom <parker.hegstrom@gmail.com>
2015-02-17 01:13:01 -0500

    user can now submit an edit to creator of the event request

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -90,6 +90,23 @@ router.put('/remove/:requestId', function (req, res, next) {
 	});
 });
 
+// route for when a shared-to user submits an edit to be approved
+router.put('/edit/:eventId', function (req, res, next) {
+	var prom = Event.findOne({_id: req.params.eventId}).exec();
+	// for POSTman
+	// req.body['editor'] = 'parker.hegstrom@gmail.com';
+	req.body['editor'] = req.session.user.email; // adds editor email to edit body
+	prom.addBack(function (err, event) {
+		Request.update({_id: event.requestID}, {$push: {edits: req.body}}, function (err, num, raw) {
+			if (err) next(err);
+			res.send('Edit sent');
+		});
+	});
+
+});
+
+
+
 router.get('/create/it', function (req, res, next) {
 	var newRequest = new Request();
 

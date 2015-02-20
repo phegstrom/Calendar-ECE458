d9edcf83676883d8fa20cd84a85c04875cd9f6d8
Peter <ypeter999@gmail.com>
2015-02-20 15:13:19 -0500

    user route for _id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,10 @@ var express = require('express');
 var User 		= require('../models/User');
 var router 		= express.Router();
 
+router.get('/:userId', function (req, res, next) {
+	res.send(req.session.user._id);
+});
+
 router.get('/:email', function (req, res, next) {
 	User.findOne({email: req.params.email}, function (err, user) {
 		res.send(user._id);

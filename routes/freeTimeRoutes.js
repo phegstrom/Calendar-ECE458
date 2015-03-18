8555ec9d6806cf4fe9f75e53bb7502197c934039
phegstrom <parker.hegstrom@gmail.com>
2015-03-18 16:06:28 -0400

    get all user ids assoc with req

--- routes/freeTimeRoutes.js
+++ routes/freeTimeRoutes.js
@@ -2,6 +2,9 @@ var express = require('express');
 var Event 		= require('../models/Event');
 var _			= require('underscore');
 var router 		= express.Router();
+var async		= require('async');
+var UserGroup 	= require('../models/UserGroup');
+var User 		= require('../models/User')
 
 router.post('/findConflicts', function (req, res, next) {
 	// timeSlots must be sorted on increasing end times
@@ -9,6 +12,24 @@ router.post('/findConflicts', function (req, res, next) {
 	// compare timeSlots to users' events with merge compare algo
 	var timeSlots = _.sortBy(req.body.timeSlots, 'endTime');
 
+	async.waterfall([
+
+		function (next) {
+			UserGroup.getUserIds(req.body.userGroupIds, function (err, ids) {
+				next(err, ids);
+			});
+		},
+		function (ids, next) {
+			User.toIds(req.body.userEmails, function (err, uids) {
+				ids = _.union(ids, uids);
+				next(err, ids);
+			});			
+		},
+		function (allIds, next) {
+			// allIds contains all user Ids associated with the req
+		}
+
+		]);
 	
 
 });

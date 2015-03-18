66cd7287c194a32ea0c790326c97e18e9898d388
Peter <ypeter999@gmail.com>
2015-03-18 15:41:45 -0400

    stuff

--- routes/freeTimeRoutes.js
+++ routes/freeTimeRoutes.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var User 		= require('../models/User');
-var PUD 		= require('../models/PUD');
 var Event 		= require('../models/Event');
-var Alert		= require('../models/Alert');
+var _			= require('underscore');
 var router 		= express.Router();
 
 router.post('/findConflicts', function (req, res, next) {
 	// timeSlots must be sorted on increasing end times
 	// events from users sorted on increasing start times
 	// compare timeSlots to users' events with merge compare algo
+	var timeSlots = _.sortBy(req.body.timeSlots, 'endTime');
+
 	
+
 });
 
 module.exports = router;

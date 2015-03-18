c44f1a0fb8be046cd7331cadcee12637c69505de
Peter <ypeter999@gmail.com>
2015-03-18 13:41:33 -0400

    1 line

--- app.js
+++ app.js
@@ -70,9 +70,7 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// first dbString is nodejitsu...
-// var dbString = 'mongodb://nodejitsu:28d2f50724f0f90b91e33914338b14c3@troup.mongohq.com:10063/nodejitsudb4576003544';
-
+// first dbString is heroku...
 var dbString = 'mongodb://heroku_app34927807:t5nfn8tkm70nlfkgngbb61k1ht@ds051960.mongolab.com:51960/heroku_app34927807';
 // developm112ent error handler
 // will print stacktrace

4d1c84ae28bf5d0f5e2c91d3cd4f69c9fc82ff67
phegstrom <parker.hegstrom@gmail.com>
2015-03-23 14:53:40 -0400

    small fix

--- routes/SlotSignUpRoutes.js
+++ routes/SlotSignUpRoutes.js
@@ -168,8 +168,8 @@ router.put('/signUp/:ssuId', function (req, res, next) {
 	//do something...create a Slot object, add to User's Slot, etc
 
 	//req.body.start, req.body.end
-	var startDate = new Date(req.body.start);
-	var endDate = new Date(req.body.end);
+	var startDate = req.body.start;
+	var endDate = req.body.end;
 
 	SlotSignUp.findOne({_id: req.params.ssuId}, function (err, ssu) {
 		ssu.takeFreeBlocks(startDate, endDate);

12dc2d34e2db7a16a44a7d546dea40bccee34c77
Wayne You <waynexyou@gmail.com>
2015-03-24 23:11:22 -0400

    Fixing data display problem with canViewBusy.

--- public/js/index.js
+++ public/js/index.js
@@ -30,8 +30,6 @@ app.run(function($rootScope, $q, $http, $modal) {
     $http.get('/ssu').
     success(function(data, status, headers, config) {
       $rootScope.slotSignupsCreated = angular.fromJson(data);
-
-      console.log($rootScope.slotSignupsCreated);
     }).
     error(function(data, status, headers, config) {
       console.log('Could not retrieve list of created slot signup events.');
@@ -182,12 +180,6 @@ app.run(function($rootScope, $q, $http, $modal) {
     else if(event) {
       //DBEvent
       $rootScope.selectedEvent = event;
-    }
-
-    if($rootScope.selectedEvent.canViewEvent) {}
-    else {
-      $rootScope.selectedEvent.canViewEvent = true;
-      $rootScope.selectedEvent.canEditEvent = false;
     }
 
     //Populate request details if owner of request
@@ -470,6 +462,7 @@ app.run(function($rootScope, $q, $http, $modal) {
         dBEvent.canEditEvent = canEdit;
         dBEvent.calendarName = calendar.name;
         dBEvent.calendarId = calendar._id;
+        dBEvent.name = calendar.owner.email + '\'s Event';
       });
     }
     else {

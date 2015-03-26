632b003b2d146d8a41cf033d3dcb045b5c89ecec
Wayne You <waynexyou@gmail.com>
2015-03-26 16:17:07 -0400

    Fixed the owner being unable to see signups for attendees. It was an issue with ng-repeat within a paragraph having another repeat.

--- public/js/ssuModalController.js
+++ public/js/ssuModalController.js
@@ -10,6 +10,22 @@ app.controller('ssuModalController', function($scope, $http, $q, $modalInstance,
     $modalInstance.dismiss('cancel');
   };
 
+  $scope.convertDates = function(startDate, endDate) {
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+    if (startDate.toDateString() == endDate.toDateString()) {
+      var displayDate = startDate.toLocaleString('en-US', {weekday: 'short', month: 'long', day: 'numeric'});
+      var startTime = startDate.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
+      var endTime = endDate.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
+      return displayDate + ', ' + startTime + ' - ' + endTime;
+    }
+    else {
+      var end = endDate.toLocaleString('en-US', {weekday: 'short', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit'});
+      var start = startDate.toLocaleString('en-US', {weekday: 'short', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit'})
+      return start + ' - ' + end;
+    }
+  }
+
   $scope.addSectionToSsu = function(sectionStart, sectionEnd) {
     if($scope.ssuDetails.sections == undefined) {
       $scope.ssuDetails.sections = [];
